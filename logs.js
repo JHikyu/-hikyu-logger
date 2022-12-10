6b68468c80452782d6b1074000a6320ff2cf906b
JHikyu <therosti@gmail.com>
2022-12-10 16:41:50 +0100

    fix

--- logs.js
+++ logs.js
@@ -4,36 +4,13 @@ const path = require('path');
 const { log, error } = require('./index.js');
 const logPathsPath = path.join(__dirname, 'logPaths.json');
 
-console.log('looking for logs in', logPathsPath)
+let currentProject = 0;
 
 if(fs.existsSync(logPathsPath)) {
   const logPaths = JSON.parse(fs.readFileSync(logPathsPath, 'utf8'));
   
   for(const key in logPaths) {
-    const value = logPaths[key];
-
-    error(key);
-
-    // console.log(key, logPaths);
-
-    if(fs.existsSync(value)) {
-      
-      const readStream = fs.createReadStream(value, { encoding: 'utf8' });
-      const lines = [];
-      
-      readStream.on('data', chunk => {
-        // Split the chunk into lines and add them to the array
-        lines.push(...chunk.split('\n'));
-      });
-      
-      readStream.on('end', () => {
-        // Log the last 10 lines of the file
-        for (let i = Math.max(lines.length - 10, 0); i < lines.length; i++) {
-          console.log(`${lines[i]}`);
-        }
-      });
-
-    }
+    logProject(key);
   }
 
 }
@@ -41,4 +18,35 @@ else {
   log('No log files found :(')
   log('Try installing @hikyu/log in a project and use the useFile() function')
   log('Run that project and then try logs again!')
+}
+
+async function logProject() {
+    const value = logPaths[key];
+
+    // console.log(key, logPaths);
+
+    if(fs.existsSync(value)) {
+
+      const readStream = fs.createReadStream(value, { encoding: 'utf8' });
+      const lines = [];
+    const logLines = [];
+
+      readStream.on('data', chunk => {
+        // Split the chunk into lines and add them to the array
+        lines.push(...chunk.split('\n'));
+      });
+
+      readStream.on('end', () => {
+        // Log the last 10 lines of the file
+        for (let i = Math.max(lines.length - 10, 0); i < lines.length; i++) {
+          logLines.push(lines[i])
+        }
+      });
+
+        error(key);
+        for(let i = 0 ; i < logLines.length ; i++) {
+            console.log(logLines[i]);
+        }
+
+    }
 }
